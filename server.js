d03b278a911fe301129c55344c597625abad8cb3
DevTabibur <tobiburrohman2@gmail.com>
2022-05-04 17:31:47 +0600

    successfully data send to backend

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // @ts-nocheck
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const express = require("express");
 const cors = require("cors");
@@ -15,32 +15,36 @@ app.use(express.json());
 // mongodb atlas connect
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hc4xz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
-async function run() {
-    try {
-      await client.connect();
-      const incubatorCollection = client.db("IncubatorApp").collection("data");
-
-      const data = {name : "Topu", mail: "abc@gmail.com"}
-      const result = await incubatorCollection.insertOne(data);
-console.log(`user inserted with ${result.insertedId}`);      
-    } finally {
-    //   await client.close();
-    }
-  }
-  run().catch(console.dir);
-
-
-
-
-app.get('/', (req, res)=>{
-    res.send("HELLO WORLD!")
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    await client.connect();
+    const incubatorCollection = client.db("IncubatorApp").collection("data");
+
+
+// send data client to backend
+app.post('/add-item', async (req, res)=>{
+    const newUser= req.body;
+    console.log('adding new user', newUser);
+    const result = await incubatorCollection.insertOne(newUser)
+    res.send(result)
+})
+  } finally {
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("HELLO WORLD!");
+});
 
 // port
-app.listen(port, ()=>{
-    console.log(`server is running on ${port}`);
-})
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
