a0213dace9027c25e460d1e1dbc97c4bebe00316
DevTabibur <tobiburrohman2@gmail.com>
2022-05-04 08:08:54 +0600

    mongodb connected and working

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// @ts-nocheck
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -8,6 +11,20 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// mongodb atlas connect
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hc4xz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    console.log('db connected');
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
 
 app.get('/', (req, res)=>{
     res.send("HELLO WORLD!")
